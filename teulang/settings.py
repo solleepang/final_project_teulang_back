fdd6e84d3eac21eeac9a1f9977b3b893d18f7194
Myeongzip <shayne0914@skuniv.ac.kr>
2023-11-06 13:54:38 +0900

    Feat: 미디어 저장 경로 설정과, teulang 폴더의 url에 media 경로 설정과 user url 추가

--- teulang/settings.py
+++ teulang/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
