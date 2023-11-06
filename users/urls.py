04e5f8945cc617140b517cde55aa9a9682bbee80
bm4706 <bm4706@naver.com>
2023-11-06 19:27:16 +0900

    feat : 프로필의 urls.py 추가

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from users import views
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='sign_up_view'),
     path('login/', views.LoginView.as_view(), name='login_view'),
+    path('api/signout/', views.UserDetailView.as_view(), name='signout_view'), # 회원탈퇴 url
+    path('<int:users_id>/',views.UserDetailView.as_view(), name='myprfile_view'), # 마이페이지 및 프로필 url
+    path('<int:users_id>/update', views.UserDetailView.as_view(), name='update_view') # 회원정보수정 url
 ]
