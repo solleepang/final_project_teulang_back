a219160bbee6e5d48ac07c584fba48861ebb760a
Myeongzip <shayne0914@skuniv.ac.kr>
2023-11-06 12:57:50 +0900

    feat: User 모델 생성,  admin 커스터마이징

--- teulang/settings.py
+++ teulang/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
 ]
 
 REST_FRAMEWORK = {
@@ -116,3 +117,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
