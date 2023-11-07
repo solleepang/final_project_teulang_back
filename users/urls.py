c2b027a2609a24b74be7e17d35964b64eb4409a2
bm4706 <bm4706@naver.com>
2023-11-07 19:32:03 +0900

    docs : / 누락

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login_view'),
     path('api/signout/', views.UserDetailView.as_view(), name='signout_view'), # 회원탈퇴 url
     path('<int:user_id>/',views.UserDetailView.as_view(), name='myprfile_view'), # 마이페이지 및 프로필 url
-    path('update/<int:user_id>', views.UserDetailView.as_view(), name='update_view') # 회원정보수정 url
+    path('update/<int:user_id>/', views.UserDetailView.as_view(), name='update_view') # 회원정보수정 url
 ]
